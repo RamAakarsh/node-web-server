1dafa611f4b9bb65c540f95ef8eecd6aeb14fbb8
Ram Aakarsh Iruvanti <aakarsh1206@gmail.com>
2018-09-02 13:46:54 -0400

    Add project page

--- server.js
+++ server.js
@@ -1,29 +1,62 @@
-const express=require('express');
-const hbs=require('hbs');
-const port=process.env.PORT ||3000;    
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
-hbs.registerPartials(__dirname+'/views/partials');
-hbs.registerHelper('getCurrentYear',()=>{
-    return new Date().getFullYear();
-})
-app.use(express.static(__dirname+'/public'));
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
-app.set('view engine','hbs');
-app.get('/help',(req,res)=>{
-    // res.send({
-    //     text:'Welcome AK !',
-    //     name:'haha'
-    // });
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-    res.render('help.hbs',{
-        help:'Help Page',
-        //year:new Date().getFullYear()
-    });
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
 });
 
-app.listen(port,()=>{
-    console.log(`server is up ${port}`);
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
 
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
